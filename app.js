7184da7003cb18526dd1270912e5ba4eaa41a4aa
Nathan Tellers <nathantellers@gmail.com>
2021-06-01 19:45:39 -0700

    adds connection to mongodb

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var logger = require('morgan');
+var cors = require('cors');
 
 var facilitiesRouter = require('./routes/facilities');
 
 var app = express();
 
+require('dotenv').config();
+require('./config/database');
+
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/facilities', facilitiesRouter);
 
